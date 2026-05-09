750a696956fe31e68a4fff57cc26576e13af858e
Sabeeir Sharrma <87631627+SabeeirSharrma@users.noreply.github.com>
2026-05-09 16:54:42 +0530

    Build, upgrade, rename to DynoMEM
    Upgrade the build and rename. Bump fabric-loom, enable a Java 17 toolchain, and update the Gradle wrapper to 8.6; switch plugin repos to mavenCentral. Bump Minecraft/yarn/loader/fabric-api to 1.20.1-compatible versions and change archives_base_name to "dynomem" while disabling third-party mods. Add Modrinth Maven repository and bundle FerriteCore (maven.modrinth:ferrite-core:6.0.1-fabric). Remove numerous legacy/internal classes (custom Guava/collection helpers, fast table/cache implementations, class-define utilities, model caches, and resource reload listener) and stop initializing the ClassConstructors/jvm injection. Update client code to no longer register the model cache reload listener and rename the logger from "Hydrogen" to "DynoMEM". Also adjust the sourcesJar API usage and update mod metadata/mixins accordingly.

--- src/main/java/me/jellysquid/mods/hydrogen/client/HydrogenClient.java
+++ src/main/java/me/jellysquid/mods/hydrogen/client/HydrogenClient.java
@@ -1,6 +1,5 @@
 package me.jellysquid.mods.hydrogen.client;
 
-import me.jellysquid.mods.hydrogen.client.resource.ModelCacheReloadListener;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.minecraft.resource.ResourceType;
@@ -8,7 +7,5 @@ import net.minecraft.resource.ResourceType;
 public class HydrogenClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES)
-                .registerReloadListener(new ModelCacheReloadListener());
     }
 }
--- src/main/java/me/jellysquid/mods/hydrogen/common/HydrogenPreLaunch.java
+++ src/main/java/me/jellysquid/mods/hydrogen/common/HydrogenPreLaunch.java
@@ -1,11 +1,9 @@
 package me.jellysquid.mods.hydrogen.common;
 
-import me.jellysquid.mods.hydrogen.common.jvm.ClassConstructors;
 import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
 
 public class HydrogenPreLaunch implements PreLaunchEntrypoint {
     @Override
     public void onPreLaunch() {
-        ClassConstructors.init();
     }
 }
